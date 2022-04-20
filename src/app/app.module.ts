f70571952f67304825f8acaf24e88c93c3006182
beth nduta <bethnduta05@gmail.com>
2022-04-20 15:31:28 +0300

    made some new files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MainComponent } from './main/main.component';
+import { FormComponent } from './form/form.component';
+import { RepositoryComponent } from './repository/repository.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    MainComponent
+    MainComponent,
+    FormComponent,
+    RepositoryComponent
   ],
   imports: [
     BrowserModule
