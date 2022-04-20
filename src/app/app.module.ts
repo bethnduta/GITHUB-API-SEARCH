ca971d9e246209391465a338b250d2ce12f8d14e
beth nduta <bethnduta05@gmail.com>
2022-04-20 19:47:12 +0300

    modified profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { MainComponent } from './main/main.component';
-import { FormComponent } from './form/form.component';
-import { RepositoryComponent } from './repository/repository.component';
-import { DatePipe } from './date.pipe';
+import { ProfileComponent } from './profile/profile.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    MainComponent,
-    FormComponent,
-    RepositoryComponent,
-    DatePipe
+    ProfileComponent
   ],
   imports: [
     BrowserModule
